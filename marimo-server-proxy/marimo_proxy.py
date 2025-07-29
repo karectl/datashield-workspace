c2bdae836ac9bf91713531bb8160fbd8c15157eb
Michael Harding <m.harding@lancaster.ac.uk>
2025-07-29 16:18:35 +0100

    Handle service base path in marimo proxy

--- marimo-server-proxy/marimo_proxy.py
+++ marimo-server-proxy/marimo_proxy.py
@@ -8,6 +8,7 @@ import configparser
 def setup_marimoserver():
 	token = secrets.token_urlsafe(16)
 	newpath = os.environ.get('JUPYTERMARIMOPROXY_PATH')
+	base_url = os.environ.get("JUPYTERHUB_SERVICE_PREFIX", "{base_url}")
 	if not newpath:
 		config = configparser.ConfigParser()
 		config.read(os.path.expanduser(os.path.join('~', '.jupytermarimoproxyrc')))
@@ -17,12 +18,13 @@ def setup_marimoserver():
 		newpath = os.path.expandvars(os.pathsep.join(os.path.expanduser(x) for x in newpath.split(os.pathsep)))
 		newpath = os.pathsep.join(x for x in newpath.split(os.pathsep) if x and x not in seen and not seen.add(x) and os.path.exists(x))
 	return {
-		'command': ['marimo', 'edit', '--port', '{port}', '--base-url', '/marimo', '--token', '--token-password', token, '--headless'],
-		# 'environment': { 'PATH': newpath } if newpath else {},
+		'command': ['marimo', 'edit', '--port', '{port}', '--base-url', f'{base_url}marimo', '--token', '--token-password', token, '--headless'],
+		'environment': { 'PATH': newpath } if newpath else {},
 		'timeout': 60,
 		'absolute_url': True,
 		'request_headers_override': { 'Authorization': 'Basic ' + base64.b64encode(b' :' + token.encode()).decode() },
 		'launcher_entry': {
 			'title': 'Marimo'
+			#'path_info': 'maarimo/'
 		},
 	}
